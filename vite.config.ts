29cbc782289e3e00f985be75164700b257e923ca
Kizito Fahad <kizitofahad665@gmail.com>
2026-05-11 23:42:55 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsConfigPaths()],
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+    },
   },
 })
