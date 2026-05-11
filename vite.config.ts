374c6eb4fe0b53a77c064700a1bab3a4ca9338f5
Kizito Fahad <kizitofahad665@gmail.com>
2026-05-11 22:15:07 +0300

    Refactor Vite config to use custom Tanstack config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [
-    tanstackStart(),
-    viteReact(),
-    tailwindcss(),
-    tsConfigPaths(),
-  ],
-  build: {
-    outDir: '.vercel/output',
+  tanstackStart: {
+    server: { entry: "server" },
   },
-})
+});
